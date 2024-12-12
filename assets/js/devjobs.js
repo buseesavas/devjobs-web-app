e253a9d25c0f85e3cb7af512ffd5c4a844b8dc2e
[Buse Savaş] <busenursavas6@gmail.com>
2024-12-12 21:58:01 +0300

    details sayfası basıldı

--- assets/js/devjobs.js
+++ assets/js/devjobs.js
@@ -46,7 +46,20 @@ async function render({filter = '', location = '', fullTimeOnly = false}) {
       return filterMatch && locationMatch && contractMatch;
     })
     .map((x) => `
-      <div class="post">
+      <div class="post"
+      data-description="${x.description}"
+      data-logo="${x.logo}"
+      data-background="${x.logoBackground}"
+      data-company="${x.company}"
+      data-link="${x.website}"
+      data-time="${x.postedAt}"
+      data-contract="${x.contract}"
+      data-position="${x.position}"
+      data-location="${x.location}"
+      data-content="${x.requirements.content}"
+      data-items='${JSON.stringify(x.requirements.items)}'
+      data-roleitems='${JSON.stringify(x.role.items)}'
+      data-role="${x.role.content}">
         <div style="background-color: ${x.logoBackground}" class="logo">
           <img src="${x.logo}" alt="">
         </div>
@@ -56,6 +69,7 @@ async function render({filter = '', location = '', fullTimeOnly = false}) {
         <p class="location">${x.location}</p>
       </div>
     `).join(' ');
+  showDetail();
 }
 
 openBtn.addEventListener("click", () => {
@@ -88,6 +102,63 @@ themeSwitch.addEventListener("change", function () {
   localStorage.setItem("theme", currentTheme);
 });
 
+function showDetail() {
+  const detailsBox = document.querySelector('.detailsBox');
+  const posts = document.querySelectorAll('.post');
+  for (const post of posts) {
+    post.addEventListener("click", function() {
+      jobPosting.innerHTML = '';
+      console.log(this.dataset.logoBackground);
+      filterForm.innerHTML = '';
+      const items = JSON.parse(this.dataset.items); 
+      const roleitems = JSON.parse(this.dataset.roleitems); 
+      detailsBox.innerHTML = `
+      <div class="detailTitle">
+          <div class="titleName">
+             <div style="background-color: ${this.dataset.background}" class="detailLogo">
+               <img src="${this.dataset.logo}" alt="Company Logo">
+             </div>
+             <div class="companyLink">
+               <h4>${this.dataset.company}</h4>
+               <p>${this.dataset.company.toLowerCase()}.com</p>
+             </div>
+          </div>
+          <button class="companyBtn">
+            <a href="${this.dataset.link}">Company Site</a>
+          </button>
+        </div>
+      <div class="detailBox">
+        <div class="detailsBoxTitle">
+          <div class="titleInfo">
+            <p>${this.dataset.time}<span></span>${this.dataset.contract}</p>
+            <h1>${this.dataset.position}</h1>
+            <h5>${this.dataset.location}</h5>
+          </div>
+          <button class="applyBtn"><a href="#">Apply Now</a></button>
+        </div>
+        <div class="detail">
+         <p class="description">${this.dataset.description}</p>
+         <div class="requirements">
+           <h6>Requirements</h6>
+           <p>${this.dataset.content}</p>
+           <ul>
+             ${items?.map((x) => `<li>${x}</li>`).join('')}
+           </ul>
+         </div>
+         <div class="tasks">
+           <h6>What You Will Do</h6>
+           <p>${this.dataset.role}</p>
+           <ol>
+             ${roleitems?.map((x) => `<li>${x}</li>`).join('')}
+           </ol>
+         </div>
+        </div>
+      </div>
+      
+      `
+    });
+  }
+}
 
 render({ });
 
